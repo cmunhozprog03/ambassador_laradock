1b5d495d63c2d84402c2d11f349d38e8b1ea17cd
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-08-09 10:20:54 -0300

    Sanctun installation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(){
-        return "Help";
+    public function register(RegisterRequest $request){
+        $user = User::create($request->only('first_name', 'last_name', 'email')
+            + [
+                'password' => Hash::make($request->input('password')),
+                'is_admin' => 1,
+            ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response([
+                'error' => 'Credenciais inválidas',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        return $user;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use \App\Http\Controllers\AuthController;
 // Admin
 Route::prefix('admin')->group(function(){
   Route::post('register', [AuthController::class, 'register']);
+  Route::post('login', [AuthController::class, 'login']);
 });
 
 
